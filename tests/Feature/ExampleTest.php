943a2b01acebdbdb0773cd1dd7ca5313ee44c0b1
opmvpc <thibsix@outlook.be>
2024-02-04 05:44:12 +0100

    Update test case to assert redirect to login page

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,15 +5,21 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function testTheApplicationReturnsASuccessfulResponse(): void
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
     }
 }
